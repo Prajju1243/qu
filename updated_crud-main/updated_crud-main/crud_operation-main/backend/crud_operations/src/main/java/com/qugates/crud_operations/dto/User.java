17b3ddc846605b740a92cb6e69128d39551ff59d
Prajju1243 <158045932+Prajju1243@users.noreply.github.com>
2024-02-03 12:54:15 +0530

    Add files via upload

---
+++ updated_crud-main/updated_crud-main/crud_operation-main/backend/crud_operations/src/main/java/com/qugates/crud_operations/dto/User.java
@@ -0,0 +1,35 @@
+package com.qugates.crud_operations.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	String email;
+	String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
